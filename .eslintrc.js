c8bbf2d92586bff80f063c45c57d579e4d831122
HyungJu <sungkisa@naver.com>
2022-01-13 17:54:46 +0900

    fix: eslint와 prettier가 작동하도록

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
-}
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {},
+};
